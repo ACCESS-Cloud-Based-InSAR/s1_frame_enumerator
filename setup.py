6d4bfb0eff42b275224e532d00e3aefec3823828
cmarshak <charles.marshak@gmail.com>
2023-02-01 17:04:06 -0800

    use python >=3.8

--- setup.py
+++ setup.py
@@ -16,15 +16,17 @@ setup(
     author='ACCESS Team',
     author_email='access-cloud-based-insar@jpl.nasa.gov',
     license='Apache 2.0',
-    classifiers=[
+     classifiers=[
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
     ],
-    python_requires='~=3.10',
+    python_requires='>=3.8',
     install_requires=[
         'geopandas', 'rasterio', 'asf_search', 'tqdm', 'requests', 'dem_stitcher'
     ],
