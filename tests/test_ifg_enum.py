6e53e86816e606841cb9570b266ec70e29495c6c
cmarshak <charles.marshak@gmail.com>
2023-03-09 17:54:17 -0800

    finish date enum test

--- tests/test_ifg_enum.py
+++ tests/test_ifg_enum.py
@@ -21,12 +21,45 @@ def test_enum_dates_with_min_baseline():
     assert date_pairs_expected == date_pairs_sorted
 
 
-def test_enum_dates_with_30_day_baseline():
-    pass
+def test_enum_dates_with_31_day_baseline():
+    n_dates = 100
+    dates = sorted([datetime.date(2021, 1, 1) + datetime.timedelta(days=j)
+                    for j in range(n_dates)])
+
+    temp_baseline = 31
+    date_pairs = enumerate_dates(dates,
+                                 min_temporal_baseline_days=temp_baseline,
+                                 n_secondary_scenes_per_ref=1)
+
+    date_pairs_sorted = sorted(date_pairs)
+    # Reference (later date) comes first
+    d0 = dates[-1]
+    delta = datetime.timedelta(days=temp_baseline)
+    n_pairs = n_dates // temp_baseline
+    date_pairs_expected = sorted([(d0 - k * delta, d0 - (k + 1) * delta)
+                                  for k in range(n_pairs)])
+    assert date_pairs_expected == date_pairs_sorted
 
 
 def test_enum_dates_with_3_neighbors():
-    pass
+    dates = [datetime.date(2021, 1, 1) + datetime.timedelta(days=j)
+             for j in range(5)]
+    date_pairs = enumerate_dates(dates,
+                                 min_temporal_baseline_days=0,
+                                 n_secondary_scenes_per_ref=3)
+
+    jan_5 = dates[-1]
+    day = datetime.timedelta(days=1)
+    date_pairs_expected = [(jan_5, jan_5 - day),
+                           (jan_5, jan_5 - 2 * day),
+                           (jan_5, jan_5 - 3 * day),
+                           (jan_5 - day, jan_5 - 2 * day),
+                           (jan_5 - day, jan_5 - 3 * day),
+                           (jan_5 - day, jan_5 - 4 * day),
+                           (jan_5 - 2 * day, jan_5 - 3 * day),
+                           (jan_5 - 2 * day, jan_5 - 4 * day),
+                           (jan_5 - 3 * day, jan_5 - 4 * day)]
+    assert date_pairs_expected == date_pairs
 
 
 def test_select_valid_ifg_pairs_using_frame_and_dates():
